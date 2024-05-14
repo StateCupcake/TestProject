0a343892e5ecdf6a4d42c76e1f07d09069be48d5
KirraleeDeed <deedkirralee@gmail.com>
2024-05-14 13:36:38 +1000

    Test Script Commit

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,9 +7,9 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Test = 7;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
